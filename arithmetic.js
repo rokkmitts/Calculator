2aecce2920898441a05ab11ea43aa2982fcb858b
rokkmitts <jtc.copeland@outlook.com>
2023-10-30 23:54:15 -0400

    feature(UI:Resizing Component); Enable the calculator to resize itself as the window its in is resized. But, with user usability in mind, set limitation so that the content remains readable and functional for the end user.

--- arithmetic.js
+++ arithmetic.js
@@ -1,3 +1,7 @@
+//
+//
+// ARITHMETIC FUNCTIONS BELOW
+//
 // Addition function to return the sum of inputs regardless of their length
 const add = function (input) {
   const sum = input.reduce(function (total, num) {
@@ -8,7 +12,6 @@ const add = function (input) {
 };
 //
 //
-
 // Subtraction function to return the difference between the inputs left to right
 const subtract = function (input) {
   const difference = input.reduce(function (num1, num2) {
@@ -18,7 +21,6 @@ const subtract = function (input) {
 };
 //
 //
-
 // Multiplication function that returns the product of the numbers recieved
 const multiply = function (input) {
   const product = input.reduce(function (num1, num2) {
@@ -28,7 +30,6 @@ const multiply = function (input) {
 };
 //
 //
-
 // Divide function that returns the quotient of the numbers recieved
 const divide = function (input) {
   const quotient = input.reduce(function (num1, num2) {
@@ -36,7 +37,15 @@ const divide = function (input) {
   });
   return quotient;
 };
-
 //
-// DO NOT MODIFY CODE BELOW THIS LINE
+//
+// LINKS THIS PAGE TO THE TESTING FILE
 module.exports = { add, subtract, multiply, divide };
+//ARITHMETIC FUNCTION ABOVE
+//
+//
+
+// VARIABLES TO COLLECT EQUATIONS
+const number1 = document.querySelector("");
+const operator = document.querySelector("");
+const number2 = document.querySelector("");
