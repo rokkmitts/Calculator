806f80eda4956b370e23bab11555a9fddafd3487
rokkmitts <jtc.copeland@outlook.com>
2023-11-11 02:33:53 -0500

    Feature(UI: layout & Functionality); Build out a base level user interface for the calculators keyboard. Rig the numbers to have low level responsiveness to being clicked on (sending a notification to the console everytime their clicked). Link the operator buttons to their respected arithmic functions.

--- arithmetic.js
+++ arithmetic.js
@@ -1,7 +1,6 @@
-//
-//
-// ARITHMETIC FUNCTIONS BELOW
-//
+//ARITHMETIC FUNCTIONS BELOW //ARITHMETIC FUNCTIONS BELOW //ARITHMETIC FUNCTIONS BELOW //
+//ARITHMETIC FUNCTIONS BELOW //ARITHMETIC FUNCTIONS BELOW //ARITHMETIC FUNCTIONS BELOW //
+
 // Addition function to return the sum of inputs regardless of their length
 const add = function (input) {
   const sum = input.reduce(function (total, num) {
@@ -10,8 +9,7 @@ const add = function (input) {
   }, 0);
   return sum;
 };
-//
-//
+
 // Subtraction function to return the difference between the inputs left to right
 const subtract = function (input) {
   const difference = input.reduce(function (num1, num2) {
@@ -19,8 +17,7 @@ const subtract = function (input) {
   });
   return difference;
 };
-//
-//
+
 // Multiplication function that returns the product of the numbers recieved
 const multiply = function (input) {
   const product = input.reduce(function (num1, num2) {
@@ -28,8 +25,7 @@ const multiply = function (input) {
   });
   return product;
 };
-//
-//
+
 // Divide function that returns the quotient of the numbers recieved
 const divide = function (input) {
   const quotient = input.reduce(function (num1, num2) {
@@ -37,15 +33,61 @@ const divide = function (input) {
   });
   return quotient;
 };
-//
-//
-// LINKS THIS PAGE TO THE TESTING FILE
-module.exports = { add, subtract, multiply, divide };
-//ARITHMETIC FUNCTION ABOVE
-//
-//
 
-// VARIABLES TO COLLECT EQUATIONS
-const number1 = document.querySelector("");
-const operator = document.querySelector("");
-const number2 = document.querySelector("");
+//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//
+//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//
+
+//
+//
+/*
+
+*/
+/*
+HOW TO SECURE USER INPUT DATA?
+ANSWER:
+FOR FRONTEND ONLY PROJECTS where there are concerns about scripting attacks, a regex validation for version control can be sufficient protection
+- Always use serverside validation on data requests and the data it recieves from users (this project has no backend)
+- Remember that the frontend(HTML file) is sent to the backend(server) as a HTTP request.
+...HTTP are relative to the browser the user is using. Older browsers convert inputs to text
+- Frontend validation is only to improve the users experience and protect aganst accidental data input errors.
+-Incompataible browsers may default to user input storage as text. Nothing client side is 'safe'.
+- Always use either javascript or PHP to validate the control of the elements chosen in the HTML.
+- AN optional use case; Instead of using REGEX the indexOf method can be used for each special character; 
+IE; str.indexOf('+') !== -1 str.indexOf('#') !== -1 Etc.
+Front
+I had in mind something along the lines of: User input might get wrapped in a sort of wrapper that prevents code from using it in unsafe ways. That input might then need to be unwrapped in a particular way that makes it difficult for someone to UNKNOWINGLY do something unsafe with it. The code using it would need to ensure it was made safe for use in the context of that code before using it. Is there something like this that has been established? – 
+
+
+
+NOTES:
+UI/FrontEnd information doesnt need to be sanitized like backend information. 
+*/
+
+// DO NOT ADD CODE BELOW HERE
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// LINKS THIS PAGE TO THE TESTING js FILE
+// module.exports = { add, subtract, multiply, divide };
