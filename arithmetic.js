565c6771cfc79c65ea302b259414983454690a4a
rokkmitts <jtc.copeland@outlook.com>
2023-11-22 23:24:34 -0500

    feature(clear and equal button functionality): Build out the clear button to function by clearing every variable used to hold and build data. Make the equals button work by giving operand2 if its available and operand1 is occupied

--- arithmetic.js
+++ arithmetic.js
@@ -1,96 +1,64 @@
-// Variables for operator buttons //
-const addBTn = document.querySelector(".btn-add");
-const subtractBTn = document.querySelector(".btn-subtract");
-const divideBTn = document.querySelector(".btn-divide");
-const multiplyBTn = document.querySelector(".btn-multiply");
-
-const inputField = document.querySelector("#inputValue");
-
 // Variables for each equation constructor //
-let operand1, operator, operand2;
+let operand1, operand2, operator;
 //
 //
-// Addition function to return the sum of inputs regardless of their length
-const add = function (input) {
-  const sum = input.reduce(function (total, num) {
-    total += num;
-    return total;
-  }, 0);
-  return sum;
-};
-// Subtraction function to return the difference between the inputs left to right
-const subtract = function (input) {
-  const difference = input.reduce(function (num1, num2) {
-    return num1 - num2;
-  });
-  return difference;
-};
-// Multiplication function that returns the product of the numbers recieved
-const multiply = function (input) {
-  const product = input.reduce(function (num1, num2) {
-    return num1 * num2;
-  });
-  return product;
-};
-// Division function that returns the quotient of the numbers recieved
-const divide = function (input) {
-  const quotient = input.reduce(function (num1, num2) {
-    return num1 / num2;
-  });
-  return quotient;
-};
-
-//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//
-//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//END OF ARITHMETIC FXS//
-
-/*BELOW IS THE DEEP FUNCTIONALITY OF THE PROGRAM*/
-
-// Variable(s) for number buttons //
+//Variable for inputField
+const inputField = document.querySelector("#inputValue");
+//
+// array collecting number buttons //
 const numberBtns = Array.from(document.querySelectorAll(".numBtn"));
-// click event for number buttons
+// array collecting operator buttons //
+const operatorBtns = Array.from(document.querySelectorAll(".opBtn"));
+//
+// click eventlisteners on every number buttons //
 numberBtns.forEach((el) =>
   el.addEventListener("click", function () {
     document.querySelector("#inputValue").value += `${this.value}`;
   })
 );
-
-// variable(s) for operator buttons
-const operatorBtns = Array.from(document.querySelectorAll(".opBtn"));
-//click event for operator buttons
+//
+//  click eventlisteners on every operator button //
 operatorBtns.forEach((el) =>
   el.addEventListener("click", function () {
-    console.log(`inital: ${operand1}`);
-    operand1 = inputField.value;
-    console.log(`set: ${operand1}`);
-    inputField.value = "";
-
-    operator = `${el.name} operator`;
-
-    // switch (operator) {
-    //   case (this.el = addBTn):
-    //     operator = add();
-    //     break;
-    //   case subtractBTn:
-    //     operator = subtract();
-    //     break;
-    //   case divideBTn:
-    //     operator = divide();
-    //     break;
-    //   case multiplyBTn:
-    //     operator = multiply();
-    //     break;
-    // }
-    console.log(operator);
+    let opName = `${el.name}`;
+    starter(opName);
   })
 );
+//
+// Function for operation construction
+function operateSwitch(opName) {
+  switch (opName) {
+    case "add":
+      return add([operand1, operand2]); /* sum */
+      break;
+    case "subtract":
+      return subtract([operand1, operand2]); /* difference */
+      break;
+    case "divide":
+      return divide([operand1, operand2]); /* quotient */
+      break;
+    case "multiply":
+      return multiply([operand1, operand2]); /* product */
+      break;
+  }
+}
+//
+// Function for inital operator analysis
+const starter = function (opName) {
+  if (!operand1 && inputField.value) {
+    operator = opName;
+    operand1 = Number(inputField.value);
+    inputField.value = "";
+  }
 
-// Operate function
-/*
-must take in the operator, operand1 and operand2 variables.
-It must make a decision of which arithmic function to use based on the operator value.
-*/
-const operate = function () {};
+  if (operand1 && inputField.value) {
+    operand2 = Number(inputField.value);
+    const ans = operateSwitch(opName);
 
+    operand1 = ans;
+    inputField.value = ans;
+  }
+};
 //
 //
 //
@@ -102,10 +70,52 @@ const operate = function () {};
 //
 //
 //
-// CALCULATOR OPERATION VARIABLES // CALCULATOR OPERATION VARIABLES
-// CALCULATOR OPERATION VARIABLES // CALCULATOR OPERATION VARIABLES
-
 //
+//
+//
+//
+//
+//
+//
+//
+//
+// ARITHMETIC FUNCTIONS
+//add function
+const add = function (input) {
+  const sum = input.reduce(function (total, num) {
+    total += num;
+    return total;
+  });
+  console.log(`sum: ${sum}`);
+  return sum;
+};
+//
+//subtract function
+const subtract = function (input) {
+  const difference = input.reduce(function (num1, num2) {
+    return num1 - num2;
+  });
+  console.log(`difference: ${difference}`);
+  return difference;
+};
+//
+//mupltiply function
+const multiply = function (input) {
+  const product = input.reduce(function (num1, num2) {
+    return num1 * num2;
+  });
+  console.log(`product: ${product}`);
+  return product;
+};
+//
+// divide function
+const divide = function (input) {
+  const quotient = input.reduce(function (num1, num2) {
+    return num1 / num2;
+  });
+  console.log(`quotient: ${quotient}`);
+  return quotient;
+};
 //
 //
 //
@@ -117,30 +127,4 @@ const operate = function () {};
 //
 //
 // DO NOT ADD CODE BELOW HERE
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// LINKS THIS PAGE TO THE TESTING js FILE
 // module.exports = { add, subtract, multiply, divide };
